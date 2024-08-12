e08d28bdaacea43113e5e0198e00721f6f2d25c0
nischaykr <nischaykumar06@gmail.com>
2024-08-12 09:23:46 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    application.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGO DB CONNECTION FAILED",error);
+})
---
+++ src/utils/ayncHandler.js
@@ -0,0 +1,7 @@
+const ayncHandler = ( requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((error) => next(error))
+    }
+}
+
+export { asyncHandler }
